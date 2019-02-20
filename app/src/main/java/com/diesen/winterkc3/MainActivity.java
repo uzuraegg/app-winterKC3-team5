22338c4416aa3c4ae19d08aa5049fc02d0785a25
diesen <diesen.aruzu65@gmail.com>
2019-02-20 15:46:19 +0900

    Initial commit

---
+++ app/src/main/java/com/diesen/winterkc3/MainActivity.java
@@ -0,0 +1,12 @@
+package com.diesen.winterkc3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
